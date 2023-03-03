0023b81acd5f252a281bcc8293b267c7ef6d7f2f
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-03-03 14:37:24 -0600

    pass function as prop

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,40 @@
 import React, {Component} from 'react'
 
+class NameTag extends Component {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props){
+        super(props)
+    }
 
-
-
-const ParaComponent = () => {
-    
-    return (
-        <h1>This should be name</h1>
-    )
+    render(){
+        return (
+            <div>
+                <h1>{`${this.props.name} ${this.props.last}`}</h1>
+                <button onClick={this.props.onclickBtn}>click me</button>
+                
+            </div>
+        )
+    }
 }
+
+
 class MainContent extends Component {
-    constructor() {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(){
         super()
+
+        this.buttonClick = this.buttonClick.bind(this)
+        
+        
+    }
+
+    buttonClick(){
+        console.log('hello')
     }
     render(){
         return (
             <div className="main-container">
-                <ParaComponent/>
+                <NameTag name='Giuseppe' last ='Camaci' onclickBtn={this.buttonClick}/>
                 <h1>This is my very first React Project</h1>
                 <p>learning to create and render multiple components</p>
             </div>
